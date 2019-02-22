fc1b941e60525534108488dacf99ec2736f2aa71
Artur Moreira <artur.moreira@fabamaq.com>
2019-02-22 19:30:27 +0000

    more stuff

---
+++ src/dom.hpp
@@ -0,0 +1,253 @@
+#ifndef LIBDOM_DOM_HPP
+#define LIBDOM_DOM_HPP
+#include <iostream>
+#include <functional>
+#include <chrono>
+#include <thread>
+#include <vector>
+
+namespace dom {
+
+	class IReducer
+	{
+	public:
+		virtual int apply() = 0;
+	};
+
+	template<typename T>
+	class array
+	{
+	public:
+
+		struct view
+		{
+			array<T>* source = nullptr;
+			std::function<bool(T&)> selector;
+			std::function<bool(T&)> reducePred;
+		};
+
+		class reduce_view : public IReducer
+		{
+		public:
+			array<T>* source = nullptr;
+			std::function<int(T&)> reducePred;
+
+			int apply() override
+			{
+				int count = 0;
+				for (auto& v : source->container)
+				{
+					auto ret = reducePred(*v);
+					count += ret;
+				}
+
+				return count;
+			}
+		};
+
+	public:
+
+		T* at(int index)
+		{
+			return container.at(index);
+		}
+
+		void notifyDataChanged(T* value)
+		{
+
+		}
+
+		void subscribe(array<T>* observer)
+		{
+			subs.push_back(observer);
+		}
+
+		void operator=(view v)
+		{
+			// Assigning a view to the array means we become bound to the source array, based on predicate
+			sourceView = v;
+
+			// subscribe to the other to get their updates
+			v.source->subscribe(this);
+
+			if (v.selector)
+			{
+				for (auto& sourceValue : v.source->container)
+				{
+					if (v.selector(*sourceValue))
+					{
+						container.push_back(sourceValue);
+					}
+				}
+			}
+
+			if (v.reducePred)
+			{
+				applyReduce();
+			}
+		}
+
+		void applyReduce()
+		{
+			container.clear();
+
+			int count = 0;
+			for (auto& v : sourceView.source->container)
+			{
+				if (sourceView.reducePred(*v))
+					count++;
+			}
+
+		}
+
+		typename std::vector<T*>::iterator begin()
+		{
+			return container.begin();
+		}
+
+		typename std::vector<T*>::iterator end()
+		{
+			return container.end();
+		}
+
+		void push(T* value)
+		{
+			container.push_back(value);
+
+			// notify subscribers
+			for (auto& s : subs)
+			{
+				s->pushInternal(value);
+			}
+		}
+
+		void pushInternal(T* value)
+		{
+			// The observed container has just pushed something to us
+			if (sourceView.selector(*value))
+			{
+				container.push_back(value);
+			}
+		}
+
+		int size()
+		{
+			return container.size();
+		}
+
+		view select(std::function<bool(T&)> pred)
+		{
+			// Returns a view over THIS container, based on the predicate
+			view v;
+			v.source = this;
+			v.selector = pred;
+			return v;
+		}
+
+		std::shared_ptr<reduce_view> reduce(std::function<int(T&)> pred)
+		{
+			std::shared_ptr<reduce_view> v = std::make_shared<reduce_view>();
+			v->source = this;
+			v->reducePred = pred;
+			return v;
+		}
+
+	private:
+		std::vector<T*>        container;
+		std::vector<array<T>*> subs;
+		view                   sourceView;
+	};
+
+
+	template<typename T>
+	class property
+	{
+	public: //API
+
+		void set(T newValue)
+		{
+			_val = newValue;
+
+			dispatchNewValue(newValue, nullptr);
+
+			if (source)
+			{
+				source->dispatchNewValue(newValue, this);
+			}
+		}
+
+	public:
+
+		void dispatchNewValue(T value, property<T>* ignore)
+		{
+			_val = value;
+
+			for (auto& s : subscribers)
+			{
+				if (s != ignore)
+				{
+					s->_val = value;
+				}
+			}
+		}
+
+		void applyReduce()
+		{
+			int count = 0;
+			for (auto& v : sourceView.source->container)
+			{
+				if (sourceView.reducePred(*v))
+					count++;
+			}
+			_val = count;
+		}
+
+		void operator=(std::shared_ptr<IReducer> reduceView)
+		{
+			// sourceView = view;
+
+			_val = reduceView->apply();
+		}
+
+		void operator=(property<T>& value)
+		{
+			// Create the channel
+			source = &value;
+			value.subscribers.push_back(this);
+
+			_val = source->_val;
+		}
+
+		bool operator==(T other)
+		{
+			return _val == other;
+		}
+
+		T& operator*() {
+			return _val;
+		}
+
+		T _val;
+		property<T>* source = nullptr;
+		std::vector<property<T>*> subscribers;
+		typename array<T>::view sourceView;
+	};
+
+
+	template<typename T>
+	class object
+	{
+	public:
+
+		T* operator->()
+		{
+			return _val;
+		}
+
+		T* _val = new T();
+	};
+
+}
+
+
+#endif //LIBDOM_DOM_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,22 @@
-#include <iostream>
-#include <functional>
-#include <chrono>
-#include <thread>
-#include <vector>
-
-namespace dom {
-
-template<typename T>
-class property
-{
-public: //API
-
-    void set(T newValue)
-    {
-        _val = newValue;
-
-        dispatchNewValue(newValue, nullptr);
-
-        if (source)
-        {
-            source->dispatchNewValue(newValue, this);
-        }
-    }
-
-public:
-
-    void dispatchNewValue(T value, property<T>* ignore)
-    {
-        _val = value;
-
-        for (auto& s : subscribers)
-        {
-            if (s != ignore)
-            {
-                s->_val = value;
-            }
-        }
-    }
-
-    void operator=(property<T>& value)
-    {
-        // Create the channel
-        source = &value;
-        value.subscribers.push_back(this);
-
-        _val = source->_val;
-    }
-
-    void operator=(T value)
-    {
-       // Delete me
-    }
-
-    bool operator==(T other)
-    {
-        return _val == other;
-    }
-
-    T& operator*() {
-        return _val;
-    }
-
-    T _val;
-    property<T>* source = nullptr;
-    std::vector<property<T>*> subscribers;
-};
-
-template<typename T>
-class array
-{
-public:
-
-    int size()
-    {
-
-    }
-
-    array<T> select(std::function<bool(T&)> pred)
-    {
-        return {};
-    }
-
-    int reduce(std::function<bool(T&)> pred)
-    {
-        return {};
-    }
-};
-
-template<typename T>
-class object
-{
-public:
-
-    T* operator->()
-    {
-        return _val;
-    }
-
-    T* _val = new T();
-};
-
-}
-
-
-//////////////////////////////////////////////////////////////////////////////// Usage
+#include "dom.hpp"
 
 class Building {
 public:
+	Building(const std::string& _name)
+	{
+		name = _name;
+	}
+
     enum Type {
         Good,
         Evil
     };
 
     std::string name;
-    int type;
-    int level;
+    int type  = Good;
+    int level = 1;
 
-    int costForNext();
+    int costForNext(){}
 };
 
 class Spell {
@@ -140,13 +40,21 @@ public:
 
 class Trophy {
 public:
+    Trophy(const std::string& _name, int _type, int _levels) {
+        name = _name;
+        attained_levels = _levels;
+        type = _type;
+    }
+
     enum Type{
         Allegiance,
         Misc
     };
 
-    int type;
+    std::string name;
+    int type = Misc;
     int attained_levels;
+    int total_levels = 5;
 };
 
 class User {
@@ -154,11 +62,11 @@ public:
 
     dom::property<int> gold;
     dom::property<int> gold_per_sec;
-    int gems;
-    int rubies;
-    int assistants;
-    int faction;
-    int pending_gems;
+	dom::property<int> gems;
+	dom::property<int> rubies;
+	dom::property<int> assistants;
+	dom::property<int> faction;
+	dom::property<int> pending_gems;
 };
 
 struct Domain {
@@ -176,19 +84,19 @@ class view_model
 public:
 
     view_model() {
-        buildings_model = domain.buildings.select([](auto& b){
+        buildings_model = domain.buildings.select([](Building& b){
             return b.type == Building::Good;
         });
 
-        spells_model = domain.spells.select([this](auto& s){
+        spells_model = domain.spells.select([this](Spell& s){
             return faction == s.faction;
         });
 
-        purchased_upgrades = domain.upgrades.select([](auto& s) {
+        purchased_upgrades = domain.upgrades.select([](Upgrade& s) {
             return s.acquired == true;
         });
 
-        unpurchased_upgrades = domain.upgrades.select([](auto& s) {
+        unpurchased_upgrades = domain.upgrades.select([](Upgrade& s) {
             return s.acquired == false && s.can_acquire;
         });
 
@@ -203,36 +111,45 @@ public:
         faction = domain.user->faction;
 
         // This reduce performs summation of all trophies levels attained
-        trophies_unlocked = domain.trophies.reduce([](auto& t){
+        trophies_unlocked = domain.trophies.reduce([](Trophy& t){
             return t.attained_levels;
         });
 
-        total_trophies = domain.trophies.size();
+        //total_trophies = domain.trophies.size();
 
-        allegiance_unlocked = domain.trophies.reduce([](auto& t) {
+        allegiance_unlocked = domain.trophies.reduce([](Trophy& t) {
             return t.type == Trophy::Allegiance ? t.attained_levels : 0;
         });
 
-        allegiance_total = domain.trophies.reduce([](auto& t) {
+        allegiance_total = domain.trophies.reduce([](Trophy& t) {
             return t.type == Trophy::Allegiance;
         });
 
-        misc_unlocked = domain.trophies.reduce([](auto& t) {
+        misc_unlocked = domain.trophies.reduce([](Trophy& t) {
             return t.type == Trophy::Misc ? t.attained_levels : 0;
         });
 
-        misc_total = domain.trophies.reduce([](auto& t) {
-            return t.type == Trophy::Misc;
+        misc_total = domain.trophies.reduce([](Trophy& t) {
+            return t.type == Trophy::Misc ? t.total_levels : 0;
         });
 
-        allegiance_trophies = domain.trophies.select([](auto& t){
+        allegiance_trophies = domain.trophies.select([](Trophy& t){
             return t.type == Trophy::Allegiance;
         });
 
-        misc_trophies = domain.trophies.select([](auto& t){
+        misc_trophies = domain.trophies.select([](Trophy& t){
             return t.type == Trophy::Misc;
         });
+
+        // We need to setup a watch so that when a trophy "level" changes, we can get a callback to emit a popup
+        //domain.trophies.watch(&Trophy::attained_levels, []())
+
+        // Maybe a diff_watcher that will track changes on the target variable, so that we can then query it
+
+        // static_watch could be just to invoke it manually and watch the dynamic ??
     }
+
+    //dom::composite_watch trophies_earned_watch;
 
     dom::property<int>   gold;
     dom::property<int>   gold_per_sec;
@@ -269,12 +186,24 @@ public:
         // Creates a two way bridge with the central variable
         gold_per_sec = domain.user->gold_per_sec;
 
+		buildings_model = domain.buildings.select([](Building& b) {
+			return b.type == Building::Good;
+		});
+
         // Binds the property so that when roto changes, we compute the expression and commit back to the others
         //gold_per_sec.bind(roto + 1);
 
         // Considering bind on expression, bind on safe expression(variables can die and its still valid),
         // bind on predicate with manual .eval() and bind on predicate with automatic eval on a watch list (pass a second arg with them)
 
+
+        // The warden trophy level property must be bound to the prison Building object, therefore we need to observe changes in a specific Building object
+
+        /*warden = dom::observe(prison, [](Building&){
+            // something about prison has changed
+            warden.level = prison->level / 250;
+            // a notify data changed is automatically dispatched for the OBSERVER
+        });*/
     }
 
     void tick()
@@ -285,21 +214,55 @@ public:
 
         if (++counter > 8)
         {
-            gold_per_sec.set(*gold_per_sec + 1);
             counter = 0;
+
+            auto* b = buildings_model.at(0);
+            b->level++;
+            buildings_model.notifyDataChanged(b);
+
+            gold_per_sec.set(1 + b->level * 2);
         }
     }
 
+    dom::object<Trophy> warden;
+    dom::object<Building> prison; // used in trophy
+
+	dom::array<Building> buildings_model;
     dom::property<int> gold;
     dom::property<int> gold_per_sec;
 };
 
-view_model vm;
-
-void printGame() {
+void printGame(view_model& vm) {
     // Always observe through vm to simulate the GUI
     std::cout << "Gold: " << *vm.gold << std::endl;
     std::cout << "Gold per sec: " << *vm.gold_per_sec << std::endl;
+
+	std::cout << "Buildings: " << std::endl;
+	for (auto& bld : vm.buildings_model)
+	{
+		std::cout << "Building: " << bld->name << "(" << bld->level << ")" << std::endl;
+	}
+
+    std::cout << "Misc trophies: " << *vm.misc_unlocked << "/" << *vm.misc_total << std::endl;
+    for (auto& miscTrophy : vm.misc_trophies)
+    {
+        std::cout << "Trophy name: " << miscTrophy->name << std::endl;
+    }
+
+    std::cout << "Allegiance trophies: " << std::endl;
+    for (auto& alleTrophy : vm.allegiance_trophies)
+    {
+        std::cout << "Trophy name: " << alleTrophy->name << std::endl;
+    }
+}
+
+void initData() {
+    domain.trophies.push(new Trophy("Building master 01", Trophy::Misc, 1));
+    domain.trophies.push(new Trophy("Warden", Trophy::Misc, 3));
+    domain.trophies.push(new Trophy("Things", Trophy::Allegiance, 4));
+    domain.trophies.push(new Trophy("Less things", Trophy::Allegiance, 4));
+
+    domain.buildings.push(new Building("Prison"));
 }
 
 int main(int argc, char *argv[])
@@ -310,14 +273,17 @@ int main(int argc, char *argv[])
     domain.user->gold.set(10);
     domain.user->gold_per_sec.set(1);
 
+    initData();
+
+    view_model vm;
+
     // Simulate loop
     while(true) {
         gold_controller c;
         c.tick();
-        printGame();
+        printGame(vm);
 
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(2s);
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 
     return 0;
